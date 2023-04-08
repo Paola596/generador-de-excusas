a73804a432eb5d695c23ee6de0c5428445228fa9
Paola Mangelli <mangelli.paola90@gmail.com>
2023-04-08 13:19:40 -0400

    Codigo del generador de excusa

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const reload = document.getElementById('reload');
+window.onload = () => {
+    document.querySelector('#reload'). addEventListener("click",() => {
+    document.querySelector('#excuse'). innerHTML = generate();
+    });   
+};
 
-reload.addEventListener('click', _ => { // el _ es para indicar la ausencia de parametros
-    location.reload();
-});
+let generate =() => {
+    let who = ['My dog','The police', 'The tornado', 'My cat', 'My grandpa'];
+    let what = ['ate','took', 'flew', 'float', 'stole'];
+    let which = ['my homework','my car', 'my house', 'my chair', 'my cellphone'];
+    let when = ['yesterday.','this morning.', 'last night.', 'last week.'];
 
+    let whoindex = Math.floor(Math.random()*who.length);
+    let whatindex = Math.floor(Math.random()*what.length);
+    let whichindex = Math.floor(Math.random()*which.length);
+    let whenindex = Math.floor(Math.random()*when.length);
+
+    return who[whoindex]+' '+what[whatindex]+' '+which[whichindex]+' '+when[whenindex];
+
+}
